3e954366d4aa99675cda01dbc8dc23402d3063d2
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-09 15:10:38 +0530

    Updated StudentList

--- UnitTesting/src/com/training/tests/StudentListTestCase.java
+++ UnitTesting/src/com/training/tests/StudentListTestCase.java
@@ -1,13 +1,15 @@
 package com.training.tests;
 
 import com.training.samples.StudentList;
+import com.training.samples.domain.Student;
 
+import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class StudentListTestCase {
 
-	StudentList listIns = new StudentList();
+	StudentList list = null;
 
 	@Before
 	public void setUp() throws Exception {
@@ -21,15 +23,31 @@ public class StudentListTestCase {
 
 	@Test
 	public void test1() {
-		// Checking if Not Null
-		assertNotNull(listIns.getNames());
+		// Checking if Duplicate Names
+		StudentList list = new StudentList();
+		int actual = list.getNames().size();
+		assertEquals(2, actual);
 	}
 
 	@Test
-	public void test2() {
-		// Checking if Not Duplicate
-		if (listIns.getNames().size() != 2)
-			fail("Improper Set");
+	public void testStudentNotNull() {
+		// Checking if Not Null
+		StudentList list = new StudentList();
+		Object obj = list.getNames();
+		assertNotNull(obj);
 	}
 
+	@Test
+	public void testSetAcceptDuplicate() {
+		// Checking if Duplicate Student
+		StudentList list = new StudentList();
+		Student s1 = new Student(101, "Ramesh");
+		Student s2 = new Student(101, "Ramesh");
+		HashSet<Student> stud = new HashSet<Student>();
+		stud.add(s1);
+		stud.add(s2);
+		list.setList(stud);
+		int actual = list.getList().size();
+		assertEquals(1, actual);
+	}
 }
